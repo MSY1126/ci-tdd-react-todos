9b04eed3b54eb2d8869390aeec6a4eb879cdf602
tpdbs222 <tpdbs222@gmail.com>
2022-10-17 16:51:56 +0900

    Todo refactoring

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,14 +1,38 @@
-import React from "react";
+// import React from "react";
 
-// 아이디, 텍스트, done, 삭제 
+// // 아이디, 텍스트, done, 삭제 
 
-const TodoItem = ({Todo}) => {
-    const {id, text, done} = todo;
-    return (
-        <li>
-            <sapn> {text} </sapn>
-            <button>삭제</button>
-        </li>);
-};
-export default TodoItem;
+// const TodoItem = ({todo, onToggle, onRemove}) => {
+//     const {id, text, done} = todo;
+//     return (
+//         <li>
+//             <sapn style ={{
+//                     TextDecoration : done ? 'line-through' : 'done'
+//                 }}
+//                 onClick ={()=> onToggle(id)}>{text} </sapn>
+//             <button onClick={()=> onRemove(id)}>삭제</button>
+//         </li>);
+//         };
+// export default TodoItem;
 
+
+import React, { useCallback } from "react";
+
+
+const TodoItem =({todo, onToggle, onRemove}) =>{
+    const {id, text, done}= todo;
+    const toggle = useCallback(()=> onToggle(id),[onToggle, id]);
+    const remove = useCallback(()=> onRemove(id),[onRemove, id]);
+    return(
+        <div>
+            <li>
+                <span style={{
+                    textDecoration:done ? 'line-through':'none'
+                }}
+                onClick={toggle}>{text}</span>
+                <button onClick={remove}>삭제</button>
+            </li>
+        </div>
+    )
+}
+export default TodoItem
--- src/TodoItem.test.js
+++ src/TodoItem.test.js
@@ -1,39 +1,103 @@
+// import React from "react";
+// import {render, fireEvent} from "@testing-library/react";
+// import TodoItem from "./Todoitem";
+
+
+// describe('<TodoItem />', () => {
+//     const sampleTodo = {
+//         id : 1,
+//         text : 'TDD배우기',
+//         done : false,
+//     };
+//     const setup = (props = {}) => {
+//         const utils = render(<todoItem todo = {sampleTodo} {...props} />);
+//         const { getByText } = utils;
+//         const todo = sampleTodo;
+//         const span = getByText(todo.text);
+//         const button = getByText(삭제);
+//         return{
+//             ...utils,
+//             sapn,
+//             button,
+//         };
+//     };
+
+//     it('has span and button', ()=>{
+//         const {span, button} = setup();
+//         expact(span). toBeTruthy();
+//         expact(button). toBeTruthy();
+//     });
+
+//     it('shows line-Through on sapn when done is true', () => {
+//         const { span } = setup ({ todo : {...sampleTodo, done: true} });
+//         expact(span).toHaveStyle('text-decoration: line-throungt;');
+//     });
+//     it('shows line-Through on sapn when done is true', () => {
+//         const {span} = setup ({ todo : {...sampleTodo, done: false} });
+//         expact(span).toHaveStyle('text-decoration: line-throungt;');
+//     });
+//     it('calls onToggle', ()=>{
+//         const onToggle = jest.fn();
+//         const {span} = setup({onToggle});
+//         fireEvent.click(span);
+//         expact(onToggle).toBeCalledWith(sampleTodo.id);
+
+//     });
+//     it('calls onRemove', ()=>{
+//         const onRemove = jest.fn();
+//         const {span} = setup({onRemove});
+//         fireEvent.click(button);
+//         expact(onRemove).toBeCalledWith(sampleTodo.id);
+    
+//     });
+// });
+
+
 import React from "react";
-import {render, fireEvent} from "@testing-library/react";
-import TodoItem from "./Todoitem";
+import { render, fireEvent } from "@testing-library/react";
+import TodoItem from "./TodoItem";
 
-
-describe('<TodoItem />', () => {
-    const sampleTodo = {
-        id : 1,
-        text : 'TDD배우기',
-        done : false,
+describe("(<TodoItem />", () =>{
+    const sampleTodo ={
+        id: 1,
+        text: 'TDD 배우기',
+        done: false,
     };
-    const setup = (props = {}) => {
-        const utils = render(<TodoItem todo = {sampleTodo} {...props} />);
-        const { getByText } = utils;
+    const setup = (props ={}) =>{
+        const utils = render(<TodoItem todo={sampleTodo} {...props} />);
+        const {getByText} =utils;
         const todo = sampleTodo;
-        const span = getByText(Todo.text);
-        const button = getByText(삭제);
-        return{
+        const span = getByText(todo.text);
+        const button = getByText('삭제');
+        return {
             ...utils,
-            sapn,
+            span,
             button,
         };
     };
-
-    it('has span and button', ()=>{
+    it('has span and button', () =>{
         const {span, button} = setup();
-        expact(span). toBeTruthy();
-        expact(button). toBeTruthy();
+        expect(span).toBeTruthy();
+        expect(button).toBeTruthy();
+    })
+    it('show line-through on span when done is true',() => {
+        const {span} = setup({todo: {...sampleTodo, done: true}});
+        expect(span).toHaveStyle('text-decoration: line-through;');
     });
-
-    it('shows line-Through on sapn when done is true', () => {
-        const { span } = setup ({ todo : {...sampleTodo, done: true} });
-        expact(span).toHaveStyle('text-decoration: line-throungt;');
+    it('show line-through on span when done is false',() => {
+        const {span} = setup({todo: {...sampleTodo, done: false}});
+        expect(span).not.toHaveStyle('text-decoration: line-through;');
     });
-
-    it('shows line-Through on sapn when done is true', () => {
-        const {span} = setup ({ todo : {...sampleTodo, done: false} });
-        expact(span).toHaveStyle('text-decoration: line-throungt;');
+    it('call onToggle',()=>{
+        const onToggle = jest.fn();
+        const {span} = setup({onToggle});
+        fireEvent.click(span);
+        expect(onToggle).toBeCalledWith(sampleTodo.id);
+    });
+    it('call onRemove',()=>{
+        const onRemove = jest.fn();
+        const {button} = setup({onRemove});
+        fireEvent.click(button);
+        expect(onRemove).toBeCalledWith(sampleTodo.id);
+    });
 });
